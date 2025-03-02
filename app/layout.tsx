3c9dee35e78de0b500e44e1154e8549e45fdecee
maryamHabibpourr <Einesheikh_12@yahoo.com>
2025-03-02 13:37:27 +0000

    Add utility functions, components, and update layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientWrapper from "@/components/ClientWrapper";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClientWrapper>{children}</ClientWrapper>
+        <ClientWrapper>
+          <Header />
+          <main>{children}</main>
+        </ClientWrapper>
       </body>
     </html>
   );
