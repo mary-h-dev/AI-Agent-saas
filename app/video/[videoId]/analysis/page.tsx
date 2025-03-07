f5e188705889c6aba5169b13e37c6d0f7372a860
maryamHabibpourr <Einesheikh_12@yahoo.com>
2025-03-07 14:31:04 +0000

    Add Google APIs and configure Next.js image domains for YouTube

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -1,29 +1,42 @@
+"use client";
+import { FeatureFlag } from "@/features/flags";
+import Usage from "@/components/Usage";
+import { useParams } from "next/navigation";
+import YouTubeVideoDetails from "@/components/YouTubeVideoDetails";
+
 function AnalysisPage() {
-    return (
-      <div className="xl:container mx-auto px-4 md:px-0">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          
-          {/* Left Side */}
-          <div className="order-2 lg:order-1 flex flex-col gap-4 bg-white lg:border-r border-gray-200 p-6">
-            <h2 className="text-xl font-semibold">Analysis Section</h2>
-            
-            {/* YouTube video details */}
-            {/* Thumbnail Generation */}
-            {/* Title Generation */}
-            {/* Transcription */}
-            <p>Cool stuff</p>
+  const params = useParams<{ videoId: string }>();
+  const { videoId } = params;
+
+  return (
+    <div className="xl:container mx-auto px-4 md:px-0">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {/* Left Side */}
+        <div className="order-2 lg:order-1 flex flex-col gap-4 bg-white lg:border-r border-gray-200 p-6">
+          {/* Analysis Section */}
+          <div className="flex flex-col gap-4 p-4 border border-gray-200 rounded-xl">
+            <Usage
+              featureFlag={FeatureFlag.ANALYSE_VIDEO}
+              title="Analyse Video"
+            />
           </div>
-  
-          {/* Right Side */}
-          <div className="order-1 lg:order-2 lg:sticky lg:top-20 h-[500px] md:h-[calc(100vh-6rem)]">
-            <h2 className="text-xl font-semibold">AI Agent Chat Section</h2>
-            <p>Chat</p>
-          </div>
-  
+          <YouTubeVideoDetails videoId={videoId} />
+
+          {/* YouTube video details */}
+          {/* Thumbnail Generation */}
+          {/* Title Generation */}
+          {/* Transcription */}
+          <p>Cool stuff</p>
+        </div>
+
+        {/* Right Side */}
+        <div className="order-1 lg:order-2 lg:sticky lg:top-20 h-[500px] md:h-[calc(100vh-6rem)]">
+          <h2 className="text-xl font-semibold">AI Agent Chat Section</h2>
+          <p>Chat</p>
         </div>
       </div>
-    );
-  }
-  
-  export default AnalysisPage;
-  
+    </div>
+  );
+}
+
+export default AnalysisPage;
